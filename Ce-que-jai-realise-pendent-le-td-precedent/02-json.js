0664c356bd400f3e4d4947c58cfb9dc6a93da883
Barder_Lucas <bardey.estelle1@gmail.com>
2022-12-09 11:13:42 +0100

    Debut JSON (incompréhensible)

---
+++ Ce-que-jai-realise-pendent-le-td-precedent/02-json.js
@@ -0,0 +1,23 @@
+
+données : [
+{
+    "nom": "Jean",
+    "projet": "Alpine.js",
+    "url": "https://alpinejs.dev/"
+  },
+  {
+    "nom": "Maxime",
+    "projet": "Tailwind CSS",
+    "url": "https://tailwindcss.com/"
+  },
+  {
+    "nom": "Adam",
+    "projet": "Laravel",
+    "url": "https://laravel.com/"
+  }
+]
+console.log(for (const obj of données) {
+  consol.log('nom : ,obj.nom')
+  consol.log('projet : ,obj.projet')
+  consol.log('url : ,obj.url')
+})
